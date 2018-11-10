3c526e7fc4e98b7444446a6a58f9f4cdaf607846
bad4iz <bad4iz@gmail.com>
2018-11-10 22:05:35 +0400

    dist

---
+++ dist/precache-manifest.d800413c73774211fbe236d924504378.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d61461a1a84cfa0d22c6",
+    "url": "/js/chunk-vendors.8a6ccee6.js"
+  },
+  {
+    "revision": "4d5a28178d697361ff58",
+    "url": "/js/app.6439bec3.js"
+  },
+  {
+    "revision": "a7aedcb8219362cd93eb",
+    "url": "/js/about.11b709af.js"
+  },
+  {
+    "revision": "9aab53770f71aba7e6fd5867532eb5c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d61461a1a84cfa0d22c6",
+    "url": "/css/chunk-vendors.39401a06.css"
+  },
+  {
+    "revision": "4d5a28178d697361ff58",
+    "url": "/css/app.de15846a.css"
+  }
+];
